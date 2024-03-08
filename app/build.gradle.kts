3faf8122c10d21eb2feac7893075987e95ab757f
docfr10 <docfr10@yandex.ru>
2024-03-08 08:15:13 +0300

    Added a Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Room
+    implementation(libs.runtime)
+    implementation(libs.compiler)
 }
