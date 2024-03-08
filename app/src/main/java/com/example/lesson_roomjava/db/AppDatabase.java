a3f6f4558c51c7c87325ffe83e1cba6c8bef690c
docfr10 <docfr10@yandex.ru>
2024-03-08 15:03:38 +0300

    Updated a comments

--- app/src/main/java/com/example/lesson_roomjava/db/AppDatabase.java
+++ app/src/main/java/com/example/lesson_roomjava/db/AppDatabase.java
@@ -6,7 +6,9 @@ import androidx.room.RoomDatabase;
 import com.example.lesson_roomjava.db.user.User;
 import com.example.lesson_roomjava.db.user.UserDao;
 
+// Абстрактный класс базы данных Room
 @Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
+    // Метод для получения объекта доступа к данным для сущности User
     public abstract UserDao userDao();
 }
--- app/src/main/java/com/example/lesson_roomjava/db/user/User.java
+++ app/src/main/java/com/example/lesson_roomjava/db/user/User.java
@@ -3,29 +3,36 @@ package com.example.lesson_roomjava.db.user;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+// Класс сущности пользователя для базы данных Room
 @Entity(tableName = "user_table")
 public class User {
+    // Поле ID пользователя (генерируется автоматически)
     @PrimaryKey(autoGenerate = true)
     private int id;
-
+    // Поле имени пользователя
     private String name;
 
+    // Конструктор класса
     public User(String name) {
         this.name = name;
     }
 
+    // Метод для получения ID пользователя
     public int getId() {
         return id;
     }
 
+    // Метод для получения имени пользователя
     public String getName() {
         return name;
     }
 
+    // Метод для установки ID пользователя
     public void setId(int id) {
         this.id = id;
     }
 
+    // Метод для установки имени пользователя
     public void setName(String name) {
         this.name = name;
     }
